78e01cc67e1b902e1ed54914492ddec93ac0a4d3
Hudson Foote <163921199+hudsonty@users.noreply.github.com>
2024-12-20 13:19:42 -0500

    Update m.js

--- static/assets/scripts/m.js
+++ static/assets/scripts/m.js
@@ -29,16 +29,16 @@ var nav = document.querySelector(".fixed-nav-bar")
 
 if (nav) {
   var html = `
-    <div class="fixed-nav-bar-container">
-    <div class="fixed-nav-bar-right">
-      <a class="navbar-link" href="/./gm"><i class="fa-solid fa-gamepad navbar-icon"></i><an>Ga</an><an>mes</an></a>
-      <a class="navbar-link" href="/./as"><i class="fa-solid fa-phone navbar-icon"></i><an>Ap</an><an>ps</an></a>
-      <a class="navbar-link" href="/./ta"><i class="fa-solid fa-laptop navbar-icon"></i><an>Ta</an><an>bs</an></a>
-      <a class="navbar-link" href="/./st"><i class="fa-solid fa-gear navbar-icon settings-icon"></i><an>Set</an><an>tings</an></a>
+    <div class="fixed-nav-bar-container" style="display: flex; justify-content: center;">
+      <div class="fixed-nav-bar-right" style="display: flex; gap: 20px;">
+        <a class="navbar-link" href="/./gm"><i class="fa-solid fa-gamepad navbar-icon"></i><an>Ga</an><an>mes</an></a>
+        <a class="navbar-link" href="/./as"><i class="fa-solid fa-phone navbar-icon"></i><an>Ap</an><an>ps</an></a>
+        <a class="navbar-link" href="/./ta"><i class="fa-solid fa-laptop navbar-icon"></i><an>Ta</an><an>bs</an></a>
+        <a class="navbar-link" href="/./st"><i class="fa-solid fa-gear navbar-icon settings-icon"></i><an>Set</an><an>tings</an></a>
+      </div>
     </div>`
   nav.innerHTML = html
 }
-
 // Themes
 var themeid = localStorage.getItem("theme")
 themeEle = document.createElement("link")
